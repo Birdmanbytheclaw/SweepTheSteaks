8f28fed1cb956bd43369b6fc9a73b2dc5e7b9061
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-02-03 08:44:39 -0600

    Classes finished. need to work on methods

---
+++ SweepSteaks/MarketingFirm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepSteaks
+{
+    class MarketingFirm
+    {
+        private class ISweepstakesManager_Manager
+        {
+            
+        }
+         
+    }
+}
