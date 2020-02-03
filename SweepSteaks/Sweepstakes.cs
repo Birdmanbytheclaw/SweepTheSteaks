c6a62c1f445b6f63ebece70a628e7a0782e33da8
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-02-03 10:44:32 -0600

    finishing sweepstakes class and may have renamed variables so checkiing to make sure they are correct then will move on to the next class

--- SweepSteaks/Sweepstakes.cs
+++ SweepSteaks/Sweepstakes.cs
@@ -7,29 +7,42 @@ using System.Threading.Tasks;
 namespace SweepSteaks
 {
 
-    class Sweepstakes
+   public class Sweepstakes
     {
-        private Dictionary<int, string> contestants = new Dictionary<int, string>();
+        private Dictionary<int, Contestant> contestants;
         private string name;
-        public string Name;
+        public string Name
+        {
+        get
+            {
+            return name;
+            }
 
-    
+        }
+        Contestant contestant;
     public Sweepstakes(string name)
     {
-
+            this.name = name;
+            contestants = new Dictionary<int, Contestant>();
     }
     public void RegisterContestant(Contestant contestant)
     {
-
+            contestant.RegistrationNumber = (contestants.Count) + 1;
+            contestants.Add(contestant.RegistrationNumber, contestant);
     }
+
+        public void PrintInfo(Contestant contestant)
+        {
+            Console.WriteLine($"First Name: {contestant.FirstName}" +
+                $"Last Name: {contestant.LastName}" +
+                $"Email: {contestant.EmailAddress}" +
+                $"Registration Number: {contestant.RegistrationNumber}");
+        }
         public Contestant PickWinner()
         {
 
         }
 
-        public void PrintContestantInfo(Contestant contestant)
-        { 
-        
-        }
+      
 
 }   }
